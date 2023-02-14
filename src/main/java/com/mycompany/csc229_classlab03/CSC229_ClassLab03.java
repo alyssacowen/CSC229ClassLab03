9562286e939c12600a4ac6d5f55d586c261659ba
alyss <alyss@LAPTOP-HJ05TM6M.home>
2023-02-14 13:34:34 -0500

    Created an array

---
+++ src/main/java/com/mycompany/csc229_classlab03/CSC229_ClassLab03.java
@@ -0,0 +1,16 @@
+package com.mycompany.csc229_classlab03;
+import java.util.*;
+/**
+ *
+ * @author alyssacowen
+ */
+public class CSC229_ClassLab03 {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       //created an array
+       int[] arr = {55,7,20,4,88,44};
+    }
+    
+    }
+}
