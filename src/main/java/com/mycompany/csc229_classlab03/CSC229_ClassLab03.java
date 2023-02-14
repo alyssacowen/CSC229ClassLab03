ea8e10bb57bf637f1303e2464a6935f569c161f2
alyssacowen <alyssarcowen@gmail.com>
2023-02-14 14:34:18 -0500

    Created an array Created method to sort array Called arraySort in main Create print statement to print sorted array Created binary and linear searches

--- src/main/java/com/mycompany/csc229_classlab03/CSC229_ClassLab03.java
+++ src/main/java/com/mycompany/csc229_classlab03/CSC229_ClassLab03.java
@@ -8,10 +8,30 @@ public class CSC229_ClassLab03 {
 
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
+       
+       
        //here I created an array
-       int[] arr = {55,7,20,0,4,88,44,31,8};
+       int[] arr = {55,7,20,1,4,88,44,31,8};
        //here I called my arraySort method
        arraySort(arr);
+       System.out.print("Enter the value you want to search for: ");
+       int value = sc.nextInt();
+       //value searched for with binary search
+        int binary = binarySearch(arr, value);
+        if (binary != -1) {
+            System.out.println("During the binary search, your value was found at: " + binary);
+        } else {
+            System.out.println("The value you entered does not exist.");
+        }
+        //value searched for with linear search
+
+        int linear = linearSearch(arr, value);
+        if (linear != -1) {
+            System.out.println("During the linear search, your value was found at: " + linear);
+        } else {
+            System.out.println("The value you entered does not exist.");
+        }
+
     }
     //here I created a method to sort the array (chose bubble sort)
     public static void arraySort(int[] arr){
@@ -29,7 +49,39 @@ public class CSC229_ClassLab03 {
         for (int j = 0; j < arr.length; j++) {
              System.out.println(arr[j]);
         }
+         
+       
     }
+         
+    public static int binarySearch(int[] arr, int value) {
+        int min = 0;
+        int max = arr.length - 1;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (arr[mid] == value) {
+                return mid;
+            } else if (arr[mid] < value) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     
+    public static int linearSearch(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
+    
+    
+    
+
 
