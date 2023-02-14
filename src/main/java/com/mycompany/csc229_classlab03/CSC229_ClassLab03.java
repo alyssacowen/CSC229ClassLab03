145cb43d36ee272855209584c63e7382159b3a90
alyssacowen <alyssarcowen@gmail.com>
2023-02-14 14:00:23 -0500

    Created an array Created method to sort array

--- src/main/java/com/mycompany/csc229_classlab03/CSC229_ClassLab03.java
+++ src/main/java/com/mycompany/csc229_classlab03/CSC229_ClassLab03.java
@@ -11,6 +11,17 @@ public class CSC229_ClassLab03 {
        //here I created an array
        int[] arr = {55,7,20,4,88,44};
     }
-    
+    //here I created a method to sort the array (chose bubble sort)
+    public static void arraySort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr [j+1]){
+                    int temp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+        }
+    }
     }
 }
+
